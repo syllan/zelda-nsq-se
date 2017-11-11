a0305f1e11b4a5834f89b807343bf78e74d34659
Franck Jousseaume <franck.jousseaume@syllan.net>
2017-11-11 19:15:44 +0100

    Dungeon 1 (wip)

--- data/maps/dungeons/1/1f.lua
+++ data/maps/dungeons/1/1f.lua
@@ -1,20 +1,9 @@
--- Lua script of map dungeons/1/1f.
--- This script is executed every time the hero enters this map.
-
--- Feel free to modify the code below.
--- You can add more events and remove the ones you don't need.
-
--- See the Solarus Lua API documentation:
--- http://www.solarus-games.org/doc/latest
-
 local map = ...
 local game = map:get_game()
 
 -- Event called at initialization time, as soon as this map is loaded.
-function map:on_started()
-
-  -- You can initialize the movement and sprites of various
-  -- map entities here.
+function map:on_started(destination)
+  map:set_doors_open("auto_door_k", true)
 end
 
 -- Event called after the opening transition effect of the map,
