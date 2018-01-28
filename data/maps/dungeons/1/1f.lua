1e2f45cff87e18dc73e38fb7fd86f1be985bb2f4
Franck Jousseaume <franck.jousseaume@syllan.net>
2018-01-28 13:29:45 +0100

    Dungeon 1 - remove some testing scripts

--- data/maps/dungeons/1/1f.lua
+++ data/maps/dungeons/1/1f.lua
@@ -1,32 +1,24 @@
+-- Lua script of map dungeons/1/1f.
+-- This script is executed every time the hero enters this map.
+
+-- Feel free to modify the code below.
+-- You can add more events and remove the ones you don't need.
+
+-- See the Solarus Lua API documentation:
+-- http://www.solarus-games.org/doc/latest
+
 local map = ...
 local game = map:get_game()
 
 -- Event called at initialization time, as soon as this map is loaded.
-function map:on_started(destination)
-  map:set_doors_open("auto_door_k", true)
-  --[[
-  if game:get_value("trap_auto_door_k") ~= false then
-    -- TODO squeletons instead of stalfos
-  end
-  --]]
+function map:on_started()
+
+  -- You can initialize the movement and sprites of various
+  -- map entities here.
 end
 
---[[
--- trap door
-function auto_door_k:on_opened()
-  if map:get_entities_count("auto_enemy_auto_door_k") == 0 then
-    game:set_value("trap_auto_door_k", false)
-  end
-end
-function close_auto_door_k_sensor:on_activated()
-  if map:get_entities_count("auto_enemy_auto_door_k") > 0
-      and game:get_value("trap_auto_door_k") ~= false then
-    map:close_doors("auto_door_k")
-    -- TODO squeletons replaced by stalfos
-  end
-end
---]]
+-- Event called after the opening transition effect of the map,
+-- that is, when the player takes control of the hero.
+function map:on_opening_transition_finished()
 
-function auto_switch_switch_block_a:on_activated()
-  map:set_custom_crystal_state("family", false)
 end
